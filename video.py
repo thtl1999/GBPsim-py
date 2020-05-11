65f48d9fd809b4e63b9ce69faefacca9f6285db2
thtl1999 <thtl2004@hanmail.net>
2020-05-12 00:38:19 +0900

    refactoring

--- video.py
+++ video.py
@@ -482,14 +482,117 @@ class VideoFrameMaker:
             s = self.P[note['frame']]['r']
             return x, y, s
 
-# class Note:
-#     def __init__(self, note_type, lane, cur_frame):
-#         self.type = note_type
-#         self.lane = lane
-#         self.cur_frame = cur_frame
-#
-#     def is_note(self):
-#         if self.type in ['Single', 'SingleOff', 'Flick', 'Tick', 'Long', 'Skill']:
-#             return True
-#         else:
-#             return False
+class NotePositions:
+    def __init__(self):
+        pass
+
+class Constants:
+    def __init__(self, settings, metadata, difficulty, song_id):
+        self.import_song_info(difficulty, song_id)
+        self.import_metadata(metadata)
+        self.import_settings(settings)
+
+
+    def import_song_info(self, difficulty, song_id):
+        diff_table = {
+            '0': 'easy',
+            '1': 'normal',
+            '2': 'hard',
+            '3': 'expert',
+            '4': 'special'
+        }
+
+        self.DIFFICULTY = diff_table[difficulty]
+        self.DIFFICULTY_ID = difficulty
+        self.SONG_ID = song_id
+
+    def import_metadata(self, metadata):
+        self.BPMS = metadata['bpm'][self.DIFFICULTY_ID]
+        self.SONG_LENGTH = metadata['length']
+        self.SONG_NAME = metadata['musicTitle'][0]
+
+    def import_settings(self, settings):
+        self.WIDTH = settings['WIDTH']
+        self.HEIGHT = settings['HEIGHT']
+        self.FPS = settings['FPS']
+        self.LANE_SCALE = settings['LANE_SCALE']
+        self.LANE_SPACE_BOTTOM = self.LANE_SCALE * settings['BOTTOM_DISTANCE']
+        self.LANE_SPACE_TOP = self.LANE_SCALE * settings['TOP_DISTANCE']
+        self.BOTTOM_Y = round(settings['LANE_BOTTOM_RATIO'] * self.HEIGHT)
+        self.TOP_Y = int(self.BOTTOM_Y - settings['BG_LINE_HEIGHT'] * self.LANE_SCALE)
+        self.SKIP_NOTE = settings['SKIP_NOTE']
+        self.NOTE_SPEED = settings['NOTE_SPEED']
+        self.NOTE_SCALE = settings['NOTE_SCALE']
+        self.NOTE_WIDTH = settings['NOTE_WIDTH']
+        self.BAR_SCALE = settings['BAR_SCALE']
+        self.FLICK_FRAMES = settings['FLICK_FRAMES']
+        self.NOTE_SKIN_ID = settings['NOTE_SKIN_ID']
+        self.LANE_SKIN_ID = settings['LANE_SKIN_ID']
+
+        self.SONG_JACKET = 'jacket/' + self.SONG_ID + '/jacket.png'
+        self.JACKET_SCALE = settings['JACKET_SCALE']
+        self.JACKET_POSITION = settings['JACKET_POSITION']
+
+        self.FONT_SIZE = settings['FONT_SIZE']
+        self.SONG_NAME_POSITION = settings['SONG_NAME_POSITION']
+
+        self.COMBO_SCALE_A = settings['COMBO_SCALE_A']
+        self.COMBO_SCALE_B = settings['COMBO_SCALE_B']
+        self.COMBO_FRAMES = settings['COMBO_FRAMES']
+        self.COMBO_POSITION = settings['COMBO_POSITION']
+
+        self.MUSIC_EXTRA_TIME = settings['MUSIC_EXTRA_TIME']
+        self.SONG_FRAME_LENGTH = int((self.SONG_LENGTH + self.MUSIC_EXTRA_TIME) * self.FPS)
+        self.BAR_EDGE_COLOR = settings['BAR_EDGE_COLOR']
+        self.BAR_CENTER_COLOR = settings['BAR_CENTER_COLOR']
+        self.BAR_LINE_COLOR = settings['BAR_LINE_COLOR']
+
+        self.OPENCV_CODEC = settings['OPENCV_CODEC']
+        self.OPENCV_VIDEO_EXT = settings['OPENCV_VIDEO_EXT']
+        self.FFMPEG_CODEC = settings['FFMPEG_CODEC']
+        self.OUTPUT_BITRATE = settings['OUTPUT_BITRATE']
+        self.OUTPUT_EXT = settings['OUTPUT_EXT']
+        self.VIDEO_NAME = str(self.SONG_ID) + self.DIFFICULTY + '.' + self.OUTPUT_EXT
+
+        if settings['THREADS'] == 0:
+            self.THREADS = multiprocessing.cpu_count()
+        else:
+            self.THREADS = settings['THREADS']
+
+class Note:
+    def __init__(self, constants, note_type, lane, cur_frame):
+        self.c = constants
+        self.type = note_type
+        self.lane = lane
+        self.cur_frame = cur_frame
+
+    def is_real_note(self):
+        if self.type in ['Single', 'SingleOff', 'Flick', 'Tick', 'Long', 'Skill']:
+            return True
+        else:
+            return False
+
+    def is_fake_note(self):
+        if self.type in ['Sim', 'Bar']:
+            return True
+        else:
+            return False
+
+    def is_note(self):
+        if self.is_real_note() or self.is_fake_note():
+            return True
+        else:
+            return False
+
+    def is_effect(self):
+        if self.type in ['Combo']:
+            return True
+        else:
+            return False
+
+    def get_pos(self):
+        if not self.is_note():
+            print(self.type, 'is not a note')
+            exit(1)
+
+
